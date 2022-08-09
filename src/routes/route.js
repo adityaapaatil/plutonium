ec257b504053a60c893aa818151919fec22775a0
sabiha <99863237+adityaapaatil@users.noreply.github.com>
2022-08-09 10:33:12 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -36,3 +36,45 @@ router.get('/student-details/:name', function(req, res){
 })
 
 module.exports = router;
+//1.
+
+router.get('/movies', function(req,res) {
+    res.send('["Spider Man", "No Time To Die", "The Father", "Dune", "The Suicide Squad"]')
+ });
+ 
+ //2.
+ router.get('/movies/:moviesid', function(req, res){
+     movies = ["Spider Man", "No Time To Die", "The Father", "Dune", "The Suicide Squad"]
+     let value = req.params.moviesid;
+     if (value > movies.length-1) {
+         res.send("doesn't exist")
+     }
+     else {
+         res.send(movies[value])
+     }
+ })
+ 
+ //3.
+ router.get('/films',function(req,res) {
+     res.send([ {id: 1, name: 'The Shining'}, {id: 2, name: 'Incendies'}, {id: 3, name: 'Rang de Basanti'}, {id: 4, name: 'Finding Demo'}])
+ });
+ 
+ 
+ //4.
+router.get('/films/:filmid',function(req,res) {
+     let films = [ {id: 1, name: 'The Shining'}, {id: 2, name: 'Incendies'}, {id: 3, name: 'Rang de Basanti'}, {id: 4, name: 'Finding Demo'}]
+     let value =req.params.filmid;
+     let found = false;
+     for(i=0; i<films.length; i++){
+         if (films[i].id==value){
+             found=true
+             res.send(films[i])
+             break;
+         }
+     }
+     if (found == false){
+         res.send("No films exists with this id")
+     }
+    });
+
+ module.exports = router;
---
+++ ss
@@ -0,0 +1,13 @@
+  assignment/nodejs-modules[m
+  main[m
+* [32msession/get-api[m
+  session/nodejs-intro[m
+  [31mremotes/origin/HEAD[m -> origin/main
+  [31mremotes/origin/account[m
+  [31mremotes/origin/demo[m
+  [31mremotes/origin/main[m
+  [31mremotes/origin/payments[m
+  [31mremotes/origin/session/get-api[m
+  [31mremotes/origin/session/nodejs-intro[m
+  [31mremotes/origin/session/nodejs-modules[m
+  [31mremotes/origin/session/query-param[m
